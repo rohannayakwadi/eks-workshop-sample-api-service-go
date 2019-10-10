75c80868f97e328c3494ceb49267915be1d20abe
rohan <rohannayakwadi@gmail.com>
2019-10-10 15:35:27 -0500

    Update main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello Rohan!!"}
+		res := &response{Message: "Welcome to CodeBuild!!"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
